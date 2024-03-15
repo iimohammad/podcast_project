4e65db65bf1dca72065e3d4090da96084589056a
Mohammad Baharlou <mohammadbaharloo96@gmail.com>
2024-03-16 01:59:46 +0330

    modify settings

--- podcastPro/settings.py
+++ podcastPro/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+from podcastPro.local_settings import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -7,13 +7,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4n^ljwx***&6wb&awe&^l_j3=sq3nuvzobovuiju6m1rw7a7jf'
+SECRET_KEY = SECRET_Key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ALLOWED_HOSTS
 
 
 # Application definition
