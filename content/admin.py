16c2efe361606f4f28185f1096548f1e3dd6a373
Mohammad Baharlou <mohammadbaharloo96@gmail.com>
2024-03-16 18:32:09 +0330

    update admin.py

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Episode
 
-# Register your models here.
+@admin.register(Episode)
+class EpisodeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'channel', 'author', 'created_at')
+    list_filter = ('channel', 'author', 'created_at')
+    search_fields = ('title', 'description')
+    date_hierarchy = 'created_at'
--- user_panel/admin.py
+++ user_panel/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Profile, Channel
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'phone_number', 'is_staff')
+    search_fields = ('username', 'email', 'phone_number')
+    readonly_fields = ('date_joined', 'last_login')
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'description')
+    search_fields = ('user__username', 'description')
+
+@admin.register(Channel)
+class ChannelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'owner')
+    search_fields = ('name', 'description', 'owner__username')
